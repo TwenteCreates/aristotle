70ec16236c89c949b906ef1fe1e8f926d12ef324
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2018-12-01 00:41:10 +0100

    Add admin page

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,6 +1,9 @@
 <template>
-    <main>
-        Admin page for managing all the entitities involved.
+    <main class="container">
+      <h1 class="title">Admin dashboard</h1>
+      <h2 class="subtitle">
+        Admin page for managing all the entitites involved.
+      </h2>
     </main>
 </template>
 

b79b029a28f0468f37eb0ee1287b494b92d759d2
Anand Chowdhary <anandchowdhary@gmail.com>
2018-12-01 15:32:31 +0100

    Add topics

--- app/pages/learn/:subject/:id.vue
+++ app/pages/learn/:subject/:id.vue
@@ -2,11 +2,67 @@
 	<main>
 		<aside>
 			<div class="vertical-nav">
-				<a href="#"><i class="fas fa-chart-line"></i></a>
-				<a href="#" class="active"><i class="fas fa-book-reader"></i></a>
-				<a href="#"><i class="fas fa-comments"></i></a>
-				<a href="#"><i class="fas fa-cogs"></i></a>
+				<nuxt-link to="/"><i class="fas fa-chart-line"></i></nuxt-link>
+				<nuxt-link to="/" class="active"><i class="fas fa-book-reader"></i></nuxt-link>
+				<nuxt-link to="/"><i class="fas fa-comments"></i></nuxt-link>
+				<nuxt-link to="/"><i class="fas fa-cogs"></i></nuxt-link>
 			</div>
+			<ul>
+				<li>
+					<nuxt-link to="/learn/nederlands">
+						<i class="fas fa-fw fa-font"></i>
+						Nederlands
+					</nuxt-link>
+					<ul>
+						<li><nuxt-link to="/learn/nederlands/1">Lesson 1</nuxt-link></li>
+						<li><nuxt-link to="/learn/nederlands/2">Lesson 2</nuxt-link></li>
+						<li><nuxt-link to="/learn/nederlands/3">Lesson 3</nuxt-link></li>
+					</ul>
+				</li>
+				<li>
+					<nuxt-link to="/learn/engels">
+						<i class="fas fa-fw fa-font"></i>
+						Engels
+					</nuxt-link>
+					<ul>
+						<li><nuxt-link to="/learn/engels/1">Lesson 1</nuxt-link></li>
+						<li><nuxt-link to="/learn/engels/2">Lesson 2</nuxt-link></li>
+						<li><nuxt-link to="/learn/engels/3">Lesson 3</nuxt-link></li>
+					</ul>
+				</li>
+				<li>
+					<nuxt-link to="/learn/wiskunde">
+						<i class="fas fa-fw fa-calculator"></i>
+						Wiskunde
+					</nuxt-link>
+					<ul>
+						<li><nuxt-link to="/learn/wiskunde/1">Multiplication</nuxt-link></li>
+						<li><nuxt-link to="/learn/wiskunde/2">Multiplication tables</nuxt-link></li>
+					</ul>
+				</li>
+				<li>
+					<nuxt-link to="/learn/aardrijkskunde">
+						<i class="fas fa-fw fa-globe-africa"></i>
+						Aardrijkskunde
+					</nuxt-link>
+					<ul>
+						<li><nuxt-link to="/learn/aardrijkskunde/1">Lesson 1</nuxt-link></li>
+						<li><nuxt-link to="/learn/aardrijkskunde/2">Lesson 2</nuxt-link></li>
+						<li><nuxt-link to="/learn/aardrijkskunde/3">Lesson 3</nuxt-link></li>
+					</ul>
+				</li>
+				<li>
+					<nuxt-link to="/learn/geschiedenis">
+						<i class="fas fa-fw fa-landmark"></i>
+						Geschiedenis
+					</nuxt-link>
+					<ul>
+						<li><nuxt-link to="/learn/geschiedenis/1">Lesson 1</nuxt-link></li>
+						<li><nuxt-link to="/learn/geschiedenis/2">Lesson 2</nuxt-link></li>
+						<li><nuxt-link to="/learn/geschiedenis/3">Lesson 3</nuxt-link></li>
+					</ul>
+				</li>
+			</ul>
 		</aside>
 		<nav :class="`${increasing ? 'is-increasing' : ''}`">
 			🏆
@@ -17,7 +73,7 @@
 		</div>
 		<div class="content" v-else>
 			<h1>{{details.name}}</h1>
-			<div v-html="marked(details.summary)" />
+			<div class="summary-text" v-html="marked(details.summary)" />
 			<iframe :src="`https://www.youtube.com/embed/${details.video}`" />
 			<div v-if="!isHidden" :class="`card is-answer card-content has-background-white-bis ${isCorrect ? 'is-correct' : ''} ${isHidden ? 'is-invisible' : ''}`">
 				<span class="tag is-danger">Quick question</span>
@@ -71,12 +127,16 @@ export default {
 				.then(data => {
 					this.details = data.data.results[0];
 					this.loading = false;
+					setTimeout(() => {
+						["natural numbers", "real numbers", "Mathematicians", "commutative", "Rational numbers", "Complex numbers", "quaternions", "Babylonians"].forEach(importantTerms => {
+							document.querySelector(".summary-text").innerHTML = document.querySelector(".summary-text").innerHTML.replace(importantTerms, `<span class="hovercard">${importantTerms}</span>`);
+						});
+						setTimeout(() => {
+							const card = new hovercard();
+							card.setup();
+						}, 1);
+					}, 1);
 				});
-			const card = new hovercard();
-			["a", "b", "c"].forEach(importantTerms => {
-				console.log(importantTerms);
-			});
-			card.setup();
 			window.onscroll = val => {
 				let h = document.documentElement, 
 				b = document.body,
@@ -104,7 +164,8 @@ export default {
 			alert("Try again!");
 		},
 		checkAnswer() {
-			this.isCorrect = !this.isCorrect;
+			console.log("ABC");
+			this.isCorrect = true;
 			const newPoints = this.points + 30;
 			setTimeout(() => {
 				const interval = setInterval(() => {
@@ -128,11 +189,6 @@ export default {
 
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css?family=Lora");
-.hovercard {
-	border-bottom: 1px solid;
-	cursor: help;
-	color: #69e;
-}
 .progress {
 	position: fixed;
 	left: 300px; right: 0;
@@ -180,10 +236,6 @@ h1 {
 	font-family: "Lora";
 	font-weight: normal;
 	font-size: 300%;
-}
-p {
-	line-height: 1.75;
-	font-size: 115%;
 }
 iframe {
 	width: 100%;
@@ -244,6 +296,41 @@ p + .card {
 		text-align: center;
 		&.active {
 			background-color: whitesmoke;
+		}
+	}
+}
+aside ul {
+	li {
+		margin-left: 70px;
+		a {
+			display: block;
+			color: inherit;
+			padding: 1rem;
+			text-decoration: none;
+			&.nuxt-link-active,
+			&.nuxt-link-active + ul,
+			&.nuxt-link-active + ul a {
+				background-color: #fff;
+			}
+			&.nuxt-link-active + ul {
+				display: block;
+				padding-bottom: 1rem;
+				li a {
+					padding: 0 1rem;
+					&.nuxt-link-exact-active {
+						font-weight: bold;
+					}
+				}
+			}
+		}
+		ul {
+			display: none;
+			li {
+				margin-left: 0;
+			}
+		}
+		i {
+			margin-right: 0.25rem;
 		}
 	}
 }

e7ca5dca291cb78efe5f299907039b2151377211
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2018-12-02 03:35:12 +0100

    Profile page

--- app/pages/profile-teacher.vue
+++ app/pages/profile-teacher.vue
@@ -28,6 +28,48 @@
             </div>
         </section>
         <section class="container profile-container">
+            <div class="field">
+                <label class="label">Display name</label>
+                <div class="control has-icons-left">
+                    <input class="input" type="text" placeholder="Text input" :value="user.displayName">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control has-icons-left">
+                    <input class="input" type="email" placeholder="Email input" :value="user.email">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                    </span>
+                </div>
+            </div>
+
+
+            <div class="field">
+                <label class="label">About</label>
+                <div class="control">
+                    <textarea class="textarea" placeholder="Textarea"></textarea>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <label class="checkbox">
+                    <input type="checkbox">
+                    I agree to the <a href="#">terms and conditions</a>
+                    </label>
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button is-link is-primary">Submit</button>
+                </div>
+            </div>
         </section>
     </main>
 </template>

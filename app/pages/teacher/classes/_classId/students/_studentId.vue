236c01d83c21f5fcd45532510b573c6c87f8478d
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2018-12-02 12:39:02 +0100

    Teacher updates

--- app/pages/teacher/classes/_classId/students/_studentId.vue
+++ app/pages/teacher/classes/_classId/students/_studentId.vue
@@ -16,30 +16,96 @@
                             <h2 class="subtitle">
                                 {{studentData.email}}
                             </h2>
+                            <h1>
+                                Last activity: <br/>
+                                {{studentData.lastSignInTime}}
+                            </h1>
+                            <h1>
+                                {{studentData.points}} total points
+                            </h1>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+
         <section class="container">
-            {{studentData}}
+            <section class="container stats-container">
+                <div class="columns is-mobile">
+                    <div class="column is-4">
+                        <div class="box hero is-bold">
+                            <div class="hero-body">
+                                <div class="container">
+                                    <h1 class="title">
+                                        {{wiskunde}} %
+                                    </h1>
+                                    <h2 class="subtitle">
+                                        'Wiskunde' progress
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-4">
+                        <div class="box hero is-bold">
+                            <div class="hero-body">
+                                <div class="container">
+                                    <h1 class="title">
+                                        {{aardrijkskunde}} %
+                                    </h1>
+                                    <h2 class="subtitle">
+                                        'Aardrijkskunde' progress
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-4">
+                        <div class="box hero is-bold">
+                            <div class="hero-body">
+                                <div class="container">
+                                    <h1 class="title">
+                                        {{geschiedenis}} %
+                                    </h1>
+                                    <h2 class="subtitle">
+                                        'Geschiedenis' active teachers
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </section>
     </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.stats-container {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+
+    .title {
+        font-size: 3em;
+    }
+}
+</style>
 
 
 <script>
 import firestore from '@/services/firestore';
 
 export default {
+  layout: 'teacher',
   data () {
     return {
         classId: null,
         studentId: null,
         classData: null,
         studentData: {},
+        wiskunde: 28,
+        aardrijkskunde: 49,
+        geschiedenis: 76,
     };
   },
   mounted() {

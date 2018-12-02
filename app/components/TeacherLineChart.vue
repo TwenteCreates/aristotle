cd300adf0743043ff4dcbcf3a922b9ac44a1c496
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2018-12-02 11:20:55 +0100

    Added teacher dashboard

---
+++ app/components/TeacherLineChart.vue
@@ -0,0 +1,39 @@
+<template>
+    <line-chart style="max-height: 500px" :chart-data="chartData" :options="options"></line-chart>
+</template>
+
+
+<script>
+  import LineChart from '@/components/charts/LineChart.js'
+
+  export default {
+    components: {
+        LineChart
+    },
+    data () {
+        return {
+            options: {
+                scales:{
+                xAxes: [{
+                        display: true
+                    }]
+                },
+                legend: {
+                    display: true,
+                    position: 'bottom',
+                },
+                title: {
+                    display: true,
+                    fontSize: '18',
+					text: 'Agregated points and questions - last 5 days'
+				},
+                responsive: true,
+                maintainAspectRatio: false,
+            }
+        }
+    },
+    props: {
+        chartData: Object
+    }
+  }
+</script>

d2cd51113c91a866da88b07834a58ae00be90838
Maurits van der Vijgh <maurits@elnino.tech>
2018-12-02 01:34:17 +0100

    add custom admin for teachers

--- api/analytics/admin.py
+++ api/analytics/admin.py
@@ -1,5 +1,16 @@
+from django.utils.translation import ugettext_lazy
+from django.contrib.admin import AdminSite
 from django.contrib import admin
 from analytics.models import *
+
+
+class TeacherAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('Aristotle Teacher')
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('Aristotle Teacher')
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('Content Management System')
 
 
 class PupilAdmin(admin.ModelAdmin):
@@ -15,12 +26,14 @@ class ConceptRelationAdmin(admin.ModelAdmin):
     list_display = ['from_concept', 'level', 'to_concept']
     search_fields = ('from_concept', 'to_concept')
 
+
 class PrimarySchoolAdmin(admin.ModelAdmin):
     list_display = ['instellings_naam', 'plaatsnaam', 'provincie', 'brin_nummer', 'internetadres']
     search_fields = ('instellings_naam', 'plaatsnaam', 'provincie', 'brin_nummer', 'internetadres')
 
+teacher_admin = TeacherAdminSite()
+teacher_admin.register(Pupil, PupilAdmin)
+teacher_admin.register(Concept, ConceptAdmin)
+teacher_admin.register(ConceptRelation, ConceptRelationAdmin)
+teacher_admin.register(PrimarySchool, PrimarySchoolAdmin)
 
-admin.site.register(Pupil, PupilAdmin)
-admin.site.register(Concept, ConceptAdmin)
-admin.site.register(ConceptRelation, ConceptRelationAdmin)
-admin.site.register(PrimarySchool, PrimarySchoolAdmin)
